6c66900c767d90b85f6b4fe69f21014e8e5679ce
Oleksandr-Smirnov <113234012+Oleksandr-Smirnov@users.noreply.github.com>
2023-01-22 22:10:08 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.getElementById('ingredients');
+console.log(ulEl);
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = ingredient;
+  ulEl.appendChild(liEl);
+}
